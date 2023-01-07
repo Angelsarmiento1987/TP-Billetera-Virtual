bddd9f249ab7c75d23c627ffe280e4fd37f57730
unknown <angel_sarm@hotmail.com>
2023-01-07 17:00:28 -0300

    15CommitTP

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 /*asignacion de Variables header*/ 
 let disponible = document.getElementById("disponible")
+let totalIngresos = document.getElementById("totalIngresos")
+let totalGastos = document.getElementById("totalGastos")
+
 
 let dinero = 0
+let monto = 0
+let gasto =``
+let gastosSumador = 0
 
 
 
@@ -10,4 +16,23 @@ function haceClick(){/*genere la funcion q disparon con el evento onclick en la 
     dinero = parseFloat(idInputDinero)
   
     disponible.innerText=`$${dinero}`
+    totalIngresos.innerText = `$${dinero}`
 }
+
+
+function agregarDinero(){
+let idInputGasto = document.getElementById("idInputGasto").value;
+let idInputMonto = document.getElementById("idInputMonto").value;
+gasto = idInputGasto
+monto = parseFloat(idInputMonto)
+dinero = dinero - monto
+gastosSumador = gastosSumador + monto
+disponible.innerText=`$${dinero}`
+totalGastos.innerText = `$${gastosSumador}`
+if(dinero<0){
+disponible.style.color = `red`
+}
+
+
+}
+
