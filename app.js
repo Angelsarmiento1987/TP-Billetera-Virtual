388b5741b2a1ca212f6b002cca68d7eac779795c
unknown <angel_sarm@hotmail.com>
2023-01-08 14:27:27 -0300

    21commitTP

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ let totalGastos = document.getElementById("totalGastos")
 let idDivGastos = document.getElementById("idDivGastos")
 
 
+
 let dinero = 0
 let monto = 0
 let gasto =``
 let gastosSumador = 0
 let sumX = 1
+
 
 
 
@@ -21,6 +23,7 @@ function haceClick(){/*genere la funcion q disparon con el evento onclick en la 
     totalIngresos.innerText = `$${dinero}`
 }
 
+/*FUNCION AGREGAR DINERO*/ 
 
 function agregarDinero(){
 let idInputGasto = document.getElementById("idInputGasto").value;
@@ -34,14 +37,27 @@ totalGastos.innerText = `$${gastosSumador}`
 if(dinero<0){
 disponible.style.color = `red`
 }
-sumX = sumX+1
-idDivGastos.insertAdjacentHTML("beforeend"/*este dato me da la ubicacion del elemento (antes o posterior)*/,`<div class="gasto" id="idItemGasto">
+idDivGastos.insertAdjacentHTML("beforeend"/*este dato me da la ubicacion del elemento (antes o posterior)*/,`<div class="gasto" id="idItemGasto${sumX}"  >
 <h3 class="gastoRealizado">${gasto}</h3>
 <h3 class="precio">${monto}</h3>
-<a href="#" class="iconoTacho"><i class="fa-regular fa-trash-can iconoEliminar"></i></a>
+<a href="#" class="iconoTacho" id="idEliminarDiv" onclick="eliminarDIV(${sumX})"><i class="fa-regular fa-trash-can iconoEliminar"></i></a>
 
 </div>`) //con insertAdjacentHTML coloco el CONTENIDO HTML uno despues de otro sin que se superpongan como con innerhtml
+
+
+
 document.getElementById("idInputGasto").value = `` /*de esta forma retorno el value de los inputs a cero/null para que puedan ser editados y seguir sumando elementos sin que me figuren los anteriores en pantalla*/ 
 document.getElementById("idInputMonto").value = ``
+
+sumX = sumX + 1 //sumador para incremento y recorrido de los divs
+
 }
 
+/*FUNCION ELIMINAR DIV*/ 
+
+function eliminarDIV(a){
+    console.log("se ejecuta la funcion al apretar tacho")
+    let variable = document.getElementById(`idItemGasto${a}`)
+    variable.remove()
+// con esta funcion guardo en variable el ID idItemGasto(con su respectivo indice que se lo da sumX para lograr identificar por separado cada div ) y el evento haceClik activa la funcion elimiinarDIV(aqui manda la variable sumX guardada para saber luego a que indice corresponde)
+}
